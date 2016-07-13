c7aba860000b7d15f77ee73e6bff31d4e676864b
probablycorey <probablycorey@github.com>
2016-07-13 15:09:07 -0700

    Move app to it’s own file

---
+++ app.jsx
@@ -0,0 +1,11 @@
+const React = require('react')
+
+class App extends React.Component {
+  render() {
+    return (
+      <p>sarah</p>
+    )
+  }
+}
+
+module.exports = App
--- snoozer.js
+++ snoozer.js
@@ -1,15 +1,6 @@
+const App = require('./app')
 const React = require('react')
 const ReactDOM = require('react-dom')
-
-class App extends React.Component {
-  render() {
-    return (
-      <p>sarah</p>
-    )
-    // return React.createElement('p', {}, 'sarah')
-  }
-}
-
 
 ReactDOM.render(
   React.createElement(App, null),
