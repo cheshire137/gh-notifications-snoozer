42d5880a2f7ebd2ac7cf4ae811463a0805750374
probablycorey <probablycorey@github.com>
2016-07-14 14:11:46 -0700

    Rename Main to App

--- components/App/App.jsx
+++ components/App/App.jsx
@@ -2,7 +2,7 @@ const React = require('react')
 const Filter = require('../Filter')
 const TaskList = require('../TaskList')
 
-class Main extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>
@@ -13,4 +13,4 @@ class Main extends React.Component {
   }
 }
 
-module.exports = Main
+module.exports = App
