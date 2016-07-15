2c7a12de1373e46a184dce0c1a5e059bbfff304f
probablycorey <probablycorey@github.com>
2016-07-15 15:13:44 -0700

    Fix App/package.json

--- test/components/app-test.js
+++ test/components/app-test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-const App = require('../../components/App/App')
+const App = require('../../components/App')
 const jsdom = require('jsdom')
 const React = require('react')
 const ReactDOM = require('react-dom')
