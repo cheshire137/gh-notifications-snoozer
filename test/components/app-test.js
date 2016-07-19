deaa6c24ed93e32be1e63592213930898b42f8b5
Sarah Vessels <cheshire137@gmail.com>
2016-07-19 17:48:09 -0600

    Fix linter complaints in app-test

--- test/components/app-test.js
+++ test/components/app-test.js
@@ -6,14 +6,14 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const TestUtils = require('react-addons-test-utils')
 
-describe('App', function() {
-  before(function() {
+describe('App', () => {
+  before(() => {
     global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
     global.window = global.document.defaultView
   })
 
-  it('renders', function() {
-    const appComponent = TestUtils.renderIntoDocument(<App/>)
+  it('renders', () => {
+    const appComponent = TestUtils.renderIntoDocument(<App />)
     assert(ReactDOM.findDOMNode(appComponent))
   })
 })
