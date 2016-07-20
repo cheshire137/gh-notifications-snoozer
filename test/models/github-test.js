cd2a29848b650e91a5c52994b57a520087a6b40c
Sarah Vessels <cheshire137@gmail.com>
2016-07-20 15:51:32 -0600

    Add a test for GitHub getToken

---
+++ test/models/github-test.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const fs = require('fs')
+const path = require('path')
+
+const GitHub = require('../../src/models/github')
+
+describe('GitHub', () => {
+  describe('getToken', () => {
+    it('returns contents of .env file', () => {
+      const tokenPath = path.join(__dirname, '..', '..', '.env')
+      const expected = fs.readFileSync(tokenPath).toString()
+      assert(expected === GitHub.getToken())
+    })
+  })
+})
