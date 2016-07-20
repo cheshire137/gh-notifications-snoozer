97d9ea245986a51038b922a22b165652951ac6c8
Sarah Vessels <cheshire137@gmail.com>
2016-07-20 15:41:26 -0600

    Add Fetcher class for making JSON API calls

---
+++ src/models/fetcher.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch')
+
+module.exports = class Fetcher {
+  get(url, opts) {
+    const options = opts || {}
+    options.method = 'GET'
+    console.log('get', url, options)
+    return this.makeRequest(url, options)
+  }
+
+  makeRequest(url, opts) {
+    const options = opts || {}
+    if (typeof options.headers === 'undefined') {
+      options.headers = {}
+    }
+    return new Promise((resolve, reject) => {
+      fetch(url, options).then((response) => {
+        this.handleJsonResponse(response, url, resolve, reject)
+      })
+    })
+  }
+
+  handleJsonResponse(response, url, resolve, reject) {
+    response.json().then((json) => {
+      if (response.ok) {
+        resolve(json)
+      } else {
+        const jsonError = json
+        jsonError.url = url
+        jsonError.status = this.getStatus(response)
+        reject(jsonError)
+      }
+    }).catch((error) => {
+      console.error('failed to parse json response', error)
+      reject({ error })
+    })
+  }
+
+  getStatus(response) {
+    return `${response.status} ${response.statusText}`
+  }
+}
