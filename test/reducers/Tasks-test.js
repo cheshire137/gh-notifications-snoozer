f99225359e0b0f5ac920e2d85cf76de9f8fe17fe
probablycorey <probablycorey@github.com>
2016-07-22 15:07:26 -0700

    Fix linting errors

--- test/reducers/Tasks-test.js
+++ test/reducers/Tasks-test.js
@@ -1,19 +1,19 @@
 const assert = require('assert')
 
-const reducer = require('../../reducers/reducer')
+const reducer = require('../../src/reducers/reducer')
 
-describe('reducers', function() {
-  it('uses an empty list as the initial state', function() {
+describe('reducers', () => {
+  it('uses an empty list as the initial state', () => {
     const defaultValue = reducer(undefined, {})
     assert.deepEqual([], defaultValue)
   })
 
-  it('adds a task', function() {
+  it('TASKS_ADD', () => {
     const action = {
       type: 'TASKS_ADD',
-      task : {
-        title: 'This is the title'
-      }
+      task: {
+        title: 'This is the title',
+      },
     }
     const state = reducer(undefined, action)
     assert.deepEqual([action.task], state)
