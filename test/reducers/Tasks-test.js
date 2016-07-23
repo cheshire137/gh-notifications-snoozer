1fa396e8d1190dbcb2a6e0d4bd69782fec24616c
probablycorey <probablycorey@github.com>
2016-07-23 12:54:20 -0700

    Don’t actually write the reducer tests yet

--- test/reducers/Tasks-test.js
+++ test/reducers/Tasks-test.js
@@ -1,21 +1,4 @@
-const assert = require('assert')
-
-const reducer = require('../../src/reducers/reducer')
-
 describe('reducers', () => {
-  it('uses an empty list as the initial state', () => {
-    const defaultValue = reducer(undefined, {})
-    assert.deepEqual([], defaultValue)
-  })
-
-  it('TASKS_ADD', () => {
-    const action = {
-      type: 'TASKS_ADD',
-      task: {
-        title: 'This is the title',
-      },
-    }
-    const state = reducer(undefined, action)
-    assert.deepEqual([action.task], state)
-  })
+  it('uses an empty list as the initial state')
+  it('TASKS_ADD')
 })
