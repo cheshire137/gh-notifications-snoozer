a33618fa48c31a8d251562d8f47d38933a2ea443
Sarah Vessels <cheshire137@gmail.com>
2016-07-22 21:26:57 -0500

    Add test for GitHub getNotifications

--- test/models/github-test.js
+++ test/models/github-test.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
-
 const GitHub = require('../../src/models/github')
+const fetchMock = require('fetch-mock')
+const Config = require('../../src/config.json')
 
 describe('GitHub', () => {
   describe('getToken', () => {
@@ -10,6 +11,26 @@ describe('GitHub', () => {
       const tokenPath = path.join(__dirname, '..', '..', '.env')
       const expected = fs.readFileSync(tokenPath).toString().trim()
       assert.equal(expected, GitHub.getToken())
+    })
+  })
+
+  describe('getNotifications', () => {
+    const notifications = [{
+      id: '153473891',
+      last_read_at: '2016-07-22T19:36:40Z',
+      reason: 'team_mention',
+    }]
+
+    before(() => {
+      fetchMock.get(`${Config.githubApiUrl}/notifications`, notifications)
+    })
+
+    it('returns a list of notifications', done => {
+      const github = new GitHub()
+      github.getNotifications().then(actual => {
+        assert.deepEqual(notifications, actual)
+        done()
+      })
     })
   })
 })
