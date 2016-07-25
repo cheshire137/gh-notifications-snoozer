dad72898466b7b049f4c8180a0ee689a449b1032
probablycorey <probablycorey@github.com>
2016-07-24 17:15:18 -0700

    Actually use arrays, not objects

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,11 +1,17 @@
-const update = require('react-addons-update')
-
 const defaultTasks = []
 
 module.exports = function(tasks = defaultTasks, action) {
   switch (action.type) {
-    case 'TASKS_UPDATE':
-      return update(tasks, { $merge: action.tasks })
+    case 'TASKS_UPDATE': {
+      // There is probably a better way to do this.
+      // Any old task that isn't in the new tasks is marked as closed
+      // Any new task that isn't in the old tasks is added
+      const newIds = action.tasks.map(({ id }) => id)
+      const closedTasks = tasks
+        .filter(({ id }) => !newIds.includes(id))
+        .map((task) => Object.assign({}, task, { state: 'closed' }))
+      return [...action.tasks, ...closedTasks].sort((a,b) => a.id - b.id)
+    }
     case 'TASKS_SELECT':
       return tasks.map(task => {
         if (task.id === action.task.id) {
--- test/reducers/Tasks-test.js
+++ test/reducers/Tasks-test.js
@@ -7,24 +7,24 @@ describe('reducers', () => {
   it('uses an empty list as the initial state')
 
   it('TASKS_UPDATE', () => {
-    const initialState = {
-      1: { title: 'task' },
-      2: { title: 'more task' },
-    }
+    const initialState = [
+      { id: 1, title: 'task' },
+      { id: 2, title: 'more task' },
+    ]
 
-    const updatedTasks = {
-      2: { title: 'updated title' },
-      3: { title: 'new task' },
-    }
+    const updatedTasks = [
+      { id: 2, title: 'updated title' },
+      { id: 3, title: 'new task' },
+    ]
 
     const store = Redux.createStore(reducer, initialState)
     store.dispatch({ type: 'TASKS_UPDATE', tasks: updatedTasks })
 
-    const expectedState = {
-      1: { title: 'task' },
-      2: { title: 'updated title' },
-      3: { title: 'new task' },
-    }
+    const expectedState = [
+      { id: 1, title: 'task', state: 'closed' },
+      { id: 2, title: 'updated title' },
+      { id: 3, title: 'new task' },
+    ]
 
     assert.deepEqual(expectedState, store.getState())
   })
