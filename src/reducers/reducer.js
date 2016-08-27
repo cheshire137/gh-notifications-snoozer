c3b47439ef34fb933c1a4d2f2a671d4f1d61247d
Sarah Vessels <cheshire137@gmail.com>
2016-08-27 11:56:35 -0500

    Refactor reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,51 +1,67 @@
 import { combineReducers } from 'redux'
 
-function tasks(tasks = [], action) {
+function updateTasks(tasks, action) {
+  const tasksById = {}
+
+  // Add the existing tasks
+  tasks.forEach(task => (tasksById[task.id] = task))
+
+  // Update tasks with new values and add new tasks
+  action.tasks.forEach(task => {
+    tasksById[task.id] = Object.assign({}, tasksById[task.id], task)
+  })
+
+  const updatedTasks = Object.keys(tasksById)
+    .map(taskId => tasksById[taskId])
+    .sort((a, b) => b.updatedAt - a.updatedAt) // Sort by updatedAt DESC
+
+  return updatedTasks
+}
+
+function selectTasks(tasks, action) {
+  return tasks.map(task => {
+    if (task.id === action.task.id) {
+      return Object.assign({}, task, { isSelected: true })
+    }
+    return task
+  })
+}
+
+function deselectTasks(tasks, action) {
+  return tasks.map(task => {
+    if (task.id === action.task.id) {
+      return Object.assign({}, task, { isSelected: false })
+    }
+    return task
+  })
+}
+
+function snoozeTasks(tasks) {
+  return tasks.map(task => {
+    if (task.isSelected) {
+      return Object.assign({}, task, { snooze: true })
+    }
+    return task
+  })
+}
+
+function tasksReducer(tasks = [], action) {
   switch (action.type) {
     case 'TASKS_EMPTY':
       return []
     case 'TASKS_UPDATE':
-      const tasksById = {}
-
-      // Add the existing tasks
-      tasks.forEach(task => (tasksById[task.id] = task))
-
-      // Update tasks with new values and add new tasks
-      action.tasks.forEach(task => {
-        tasksById[task.id] = Object.assign({}, tasksById[task.id], task)
-      })
-
-      const updatedTasks = Object.keys(tasksById)
-        .map(taskId => tasksById[taskId])
-        .sort((a, b) => b.updatedAt - a.updatedAt) // Sort by updatedAt DESC
-
-      return updatedTasks
+      return updateTasks(tasks, action)
     case 'TASKS_SELECT':
-      return tasks.map(task => {
-        if (task.id === action.task.id) {
-          return Object.assign({}, task, { isSelected: true })
-        }
-        return task
-      })
+      return selectTasks(tasks, action)
     case 'TASKS_DESELECT':
-      return tasks.map(task => {
-        if (task.id === action.task.id) {
-          return Object.assign({}, task, { isSelected: false })
-        }
-        return task
-      })
+      return deselectTasks(tasks, action)
     case 'TASKS_SNOOZE':
-      return tasks.map(task => {
-        if (task.isSelected) {
-          return Object.assign({}, task, { snooze: true })
-        }
-        return task
-      })
+      return snoozeTasks(tasks)
     default:
       return tasks
   }
 }
 
 module.exports = combineReducers({
-  tasks,
+  tasks: tasksReducer,
 })
