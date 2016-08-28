035eeb28375231a6307e40bf64b8117db4668c41
Sarah Vessels <cheshire137@gmail.com>
2016-08-27 19:46:47 -0500

    Link to me and Corey on GitHub from About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,3 +1,4 @@
+const { shell } = require('electron')
 const React = require('react')
 const path = require('path')
 const packagePath = path.join(__dirname, '..', '..', '..', 'package.json')
@@ -9,6 +10,13 @@ class About extends React.Component {
     this.props.cancel()
   }
 
+  handleLinkClick(event) {
+    event.preventDefault()
+    const link = event.currentTarget
+    link.blur()
+    shell.openExternal(link.href)
+  }
+
   render() {
     return (
       <div>
@@ -17,6 +25,19 @@ class About extends React.Component {
           <span> / </span>
           About
         </h1>
+        <p>
+          {packageInfo.productName}
+          <span> was built by </span>
+          <a
+            href="https://github.com/probablycorey"
+            onClick={e => this.handleLinkClick(e)}
+          >@probablycorey</a>
+          <span> and </span>
+          <a
+            href="https://github.com/cheshire137"
+            onClick={e => this.handleLinkClick(e)}
+          >@cheshire137</a>.
+        </p>
         <p>
           <strong>Version: </strong> {packageInfo.version}
         </p>
