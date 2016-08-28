5eca97b1aba3f246792a049e90e35947c5432062
Sarah Vessels <cheshire137@gmail.com>
2016-08-27 20:01:43 -0500

    Use double quotes to contain single quotes

--- test/components/TaskList-test.js
+++ test/components/TaskList-test.js
@@ -3,18 +3,18 @@ describe('TaskList', () => {
 
   context('when the snooze button is clicked', () => {
     it('hides selected tasks')
-    it('updates the selected task\'s `snooze_until` field')
+    it("updates the selected task's `snooze_until` field")
     it('shows the tasks again, starting at midnight of the next day')
   })
 
   context('when the archive button is clicked', () => {
     it('hides selected tasks')
-    it('updates the selected task\'s `archived_at` field')
+    it("updates the selected task's `archived_at` field")
     it('shows tasks again if `updated_at` is greater than `archived_at`')
   })
 
   context('when the ignore button is clicked', () => {
     it('hides selected tasks')
-    it('updates the selected task\'s `ignored` field')
+    it("updates the selected task's `ignored` field")
   })
 })
