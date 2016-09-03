197c090358f65167b5762d2d68af4bb693d00eb2
Sarah Vessels <cheshire137@gmail.com>
2016-09-03 11:17:10 -0500

    Include repository owner user info, including avatar

--- src/models/github.js
+++ src/models/github.js
@@ -5,6 +5,40 @@ const path = require('path')
 const Config = require('../config.json')
 const Fetcher = require('./fetcher')
 
+const repoUrlPrefix = 'https://api.github.com/repos/'
+
+function getTask(data) {
+  const repoUrl = data.repository_url
+  const repository = repoUrl.slice(repoUrlPrefix.length)
+  const repositoryOwner = repository.split('/')[0]
+  return {
+    id: data.id,
+    type: typeof data.pull_request === 'object' ? 'pull' : 'issue',
+    title: data.title,
+    body: data.body,
+    state: data.state,
+    createdAt: data.created_at,
+    updatedAt: data.updated_at,
+    closedAt: data.closed_at,
+    isPullRequest: !!data.pull_request,
+    repositoryApiUrl: repoUrl,
+    url: data.html_url,
+    number: data.number,
+    repository,
+    repositoryOwner: {
+      login: repositoryOwner,
+      url: `https://github.com/${repositoryOwner}`,
+      avatarUrl: `https://github.com/${repositoryOwner}.png?size=16`,
+    },
+    user: {
+      login: data.user.login,
+      url: data.user.html_url,
+      avatarUrl: `https://github.com/${data.user.login}.png?size=16`,
+      type: data.user.type,
+    },
+  }
+}
+
 class GitHub extends Fetcher {
   // https://developer.github.com/v3/activity/notifications/#list-your-notifications
   getNotifications() {
@@ -14,33 +48,7 @@ class GitHub extends Fetcher {
   // https://developer.github.com/v3/search/#search-issues
   getTasks(query = Config.searchQuery) {
     const urlPath = `search/issues?q=${encodeURIComponent(query)}&sort=updated`
-    const repoUrlPrefix = 'https://api.github.com/repos/'
-    return this.get(urlPath).then(({ items }) => items.map(task => {
-      const repoUrl = task.repository_url
-      const avatarUrl = `https://github.com/${task.user.login}.png?size=16`
-      return {
-        id: task.id,
-        type: typeof task.pull_request === 'object' ? 'pull' : 'issue',
-        title: task.title,
-        body: task.body,
-        state: task.state,
-        createdAt: task.created_at,
-        updatedAt: task.updated_at,
-        closedAt: task.closed_at,
-        isPullRequest: !!task.pull_request,
-        repositoryApiUrl: repoUrl,
-        url: task.html_url,
-        number: task.number,
-        repository: repoUrl.slice(repoUrlPrefix.length),
-        user: {
-          login: task.user.login,
-          url: task.user.html_url,
-          avatarUrl,
-          type: task.user.type,
-        },
-      }
-    })
-    )
+    return this.get(urlPath).then(({ items }) => items.map(d => getTask(d)))
   }
 
   static getToken() {
