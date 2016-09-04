742f889183384460ab39e41f1cdf74aa020abe83
Sarah Vessels <cheshire137@gmail.com>
2016-09-04 16:58:54 -0500

    DRY up App tests

--- test/components/App-test.js
+++ test/components/App-test.js
@@ -10,6 +10,14 @@ const App = require('../../src/components/App')
 const reducer = require('../../src/reducers/reducer')
 const GitHubAuth = require('../../src/models/github-auth')
 
+function renderPage(store) {
+  return TestUtils.renderIntoDocument(
+    <ReactRedux.Provider store={store}>
+      <App />
+    </ReactRedux.Provider>
+  )
+}
+
 describe('App', () => {
   let store
 
@@ -23,21 +31,12 @@ describe('App', () => {
   })
 
   it('renders', () => {
-    const appComponent = TestUtils.renderIntoDocument(
-      <ReactRedux.Provider store={store}>
-        <App />
-      </ReactRedux.Provider>
-    )
-
+    const appComponent = renderPage(store)
     assert(ReactDOM.findDOMNode(appComponent))
   })
 
   it('does not make request without a token', () => {
-    TestUtils.renderIntoDocument(
-      <ReactRedux.Provider store={store}>
-        <App />
-      </ReactRedux.Provider>
-    )
+    renderPage(store)
 
     const fetchedCalls = fetchMock.calls().matched
     assert.equal(0, fetchedCalls.length, 'No fetch calls should be made.')
@@ -46,11 +45,7 @@ describe('App', () => {
   it('fetches user when auth token is set', () => {
     GitHubAuth.setToken('test-whee')
 
-    TestUtils.renderIntoDocument(
-      <ReactRedux.Provider store={store}>
-        <App />
-      </ReactRedux.Provider>
-    )
+    renderPage(store)
 
     const fetchedCalls = fetchMock.calls().matched
     assert.equal(1, fetchedCalls.length, 'Only one fetch call should be made.')
