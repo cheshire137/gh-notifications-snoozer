4bf734bd61e6558f27c9c96c54b604843569f08a
Sarah Vessels <cheshire137@gmail.com>
2016-09-10 17:43:08 -0500

    Add some TaskList tests for showing task list items

--- test/components/TaskListTest.js
+++ test/components/TaskListTest.js
@@ -1,5 +1,77 @@
+const assert = require('assert')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const ReactRedux = require('react-redux')
+const Redux = require('redux')
+const TestUtils = require('react-addons-test-utils')
+const fetchMock = require('fetch-mock')
+
+const ElectronConfig = require('electron-config')
+const storage = new ElectronConfig({ name: 'config-test' })
+
+const Config = require('../../src/config.json')
+const App = require('../../src/components/App')
+const reducer = require('../../src/reducers/reducer')
+const GitHubAuth = require('../../src/models/GitHubAuth')
+const Filter = require('../../src/models/Filter')
+const LastFilter = require('../../src/models/LastFilter')
+const fixtures = require('../fixtures')
+
 describe('TaskList', () => {
-  it('does not show tasks that are ignored, archived or snoozed')
+  let renderedDOM
+  let store
+
+  before(() => {
+    storage.clear()
+
+    // Persist access token
+    GitHubAuth.setToken('test-whee')
+
+    // Fake responses from GitHub API
+    fetchMock.get(`${Config.githubApiUrl}/user`, { login: 'testuser123' })
+    fetchMock.get(`${Config.githubApiUrl}/search/issues?q=cats`,
+                  { items: [fixtures.pullRequest] })
+
+    // Persist a filter
+    const filter = new Filter('Cool name')
+    filter.store('cats')
+    LastFilter.save('Cool name')
+
+    store = Redux.createStore(reducer, { tasks: [] })
+    const component = TestUtils.renderIntoDocument(
+      <ReactRedux.Provider store={store}>
+        <App />
+      </ReactRedux.Provider>
+    )
+    renderedDOM = () => ReactDOM.findDOMNode(component)
+  })
+
+  it('shows task that is not snoozed, archived, or ignored', () => {
+    assert(renderedDOM().querySelector('#pull-163031382'))
+  })
+
+  it('does not show task that is ignored', () => {
+  })
+
+  it('does not show task that is archived', () => {
+    store.dispatch({ type: 'TASKS_SELECT', task: {
+      storageKey: 'pull-163031382',
+    } })
+    store.dispatch({ type: 'TASKS_ARCHIVE' })
+
+    const taskListItems = renderedDOM().querySelectorAll('.task-list-item')
+    assert.equal(0, taskListItems.length)
+  })
+
+  it('does not show task that is snoozed', () => {
+    store.dispatch({ type: 'TASKS_SELECT', task: {
+      storageKey: 'pull-163031382',
+    } })
+    store.dispatch({ type: 'TASKS_SNOOZE' })
+
+    const taskListItems = renderedDOM().querySelectorAll('.task-list-item')
+    assert.equal(0, taskListItems.length)
+  })
 
   context('when the snooze button is clicked', () => {
     it('hides selected tasks')
