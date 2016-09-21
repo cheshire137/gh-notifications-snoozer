0487637f9a8ceb7135957e678bae973a94e150c3
Summer Smith <jessica.summer.smith@gmail.com>
2016-09-21 15:28:51 -0400

    Add test for submitting New Filter

--- test/components/NewFilterTest.js
+++ test/components/NewFilterTest.js
@@ -1,15 +1,40 @@
 const assert = require('assert')
 const React = require('react')
 const ReactDOM = require('react-dom')
+const ReactRedux = require('react-redux')
+const Redux = require('redux')
+const fetchMock = require('fetch-mock')
 const TestUtils = require('react-addons-test-utils')
-const NewFilter = require('../../src/components/NewFilter/NewFilter.jsx')
+const NewFilter = require('../../src/components/NewFilter')
+const ElectronConfig = require('electron-config')
+const storage = new ElectronConfig({ name: 'config-test' })
+const reducer = require('../../src/reducers/reducer')
+
+function renderPage(store) {
+  // NewFilter calls loadFilter and save functions from the main App onSubmit.
+  return TestUtils.renderIntoDocument(
+    <ReactRedux.Provider store={store}>
+      <NewFilter
+        loadFilter={key => key}
+        save={() => 'saved'}
+      />
+    </ReactRedux.Provider>
+  )
+}
 
 describe('NewFilter', () => {
   let renderedDOM
+  let store
 
   before(() => {
-    const component = TestUtils.renderIntoDocument(<NewFilter />)
+    store = Redux.createStore(reducer)
+    fetchMock.mock('*', {})
+    const component = renderPage(store)
     renderedDOM = () => ReactDOM.findDOMNode(component)
+  })
+
+  after(() => {
+    fetchMock.restore()
   })
 
   it('shows the form', () => {
@@ -19,17 +44,24 @@ describe('NewFilter', () => {
 
   describe('Create', () => {
     it('has a save button', () => {
-      const saveButton = renderedDOM().querySelectorAll('#new-filter-save')
-      assert.equal(1, saveButton.length)
+      const saveButton = renderedDOM().querySelector('#new-filter-save')
+      assert(saveButton)
     })
 
     it('has required form fields', () => {
-      const valueInput = renderedDOM().querySelectorAll("input[name='filterValue']")
-      assert.equal(1, valueInput.length)
+      const valueInput = renderedDOM().querySelector("input[name='filterValue']")
+      assert(valueInput)
     })
 
-    xit('sets a created filter as LastFilter', () => {
+    it('sets a created filter as LastFilter', () => {
+      const filterForm = renderedDOM().querySelector('form.new-filter-form')
+      const valueInput = renderedDOM().querySelector("input[name='filterValue']")
+      const filter = 'author:LuluPopplewell'
 
+      valueInput.value = filter
+      TestUtils.Simulate.change(valueInput)
+      TestUtils.Simulate.submit(filterForm)
+      assert.equal(storage.get(filter), 'author:LuluPopplewell')
     })
   })
 })
