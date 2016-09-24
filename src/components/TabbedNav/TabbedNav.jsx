f9065bb660d3c58f45e2b325b8d3773b609fbc8a
Sarah Vessels <cheshire137@gmail.com>
2016-09-24 15:51:38 -0500

    Shorten some lines in TabbedNav

--- src/components/TabbedNav/TabbedNav.jsx
+++ src/components/TabbedNav/TabbedNav.jsx
@@ -3,25 +3,26 @@ const hookUpStickyNav = require('../hookUpStickyNav')
 
 class TabbedNav extends React.Component {
   render() {
+    const { active, user } = this.props
     return (
       <nav id="tabbed-nav" className="top-nav nav tabs is-toggle is-fullwidth">
         <ul>
-          <li className={this.props.active === 'tasks' ? 'is-active' : ''}>
+          <li className={active === 'tasks' ? 'is-active' : ''}>
             <a id="notifications-link" onClick={this.props.showTasks}>
               <span className="tab octicon octicon-mail"></span>
               <span>Tasks</span>
             </a>
           </li>
-          <li className={this.props.active === 'filters' ? 'is-active' : ''}>
+          <li className={active === 'filters' ? 'is-active' : ''}>
             <a id="filters-link" onClick={this.props.manageFilters}>
               <span className="tab octicon octicon-beaker"></span><span>Filters</span>
             </a>
           </li>
-          <li className={this.props.active === 'auth' ? 'is-active' : ''}>
-            {typeof this.props.user === 'object' ? (
+          <li className={active === 'auth' ? 'is-active' : ''}>
+            {typeof user === 'object' ? (
               <a id="auth-link" onClick={this.props.showAuth}>
                 <span className="tab octicon octicon-mark-github"></span>
-                <span className="user-login">{this.props.user.login}</span>
+                <span className="user-login">{user.login}</span>
               </a>
             ) : ''}
           </li>
