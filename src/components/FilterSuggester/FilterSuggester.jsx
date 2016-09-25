5ff9f8e6948ad76c698f4d6ce85902db20943c46
Sarah Vessels <cheshire137@gmail.com>
2016-09-25 15:51:03 -0500

    Suggest values for the filters that have limited values

--- src/components/FilterSuggester/FilterSuggester.jsx
+++ src/components/FilterSuggester/FilterSuggester.jsx
@@ -6,16 +6,29 @@ const filters = [
     name: 'type:',
     tip: 'issue, pr',
     group: 'general',
+    suggestions: [
+      { name: 'issue', prefix: 'type:' },
+      { name: 'pr', prefix: 'type:' },
+    ],
   },
   {
     name: 'in:',
     tip: 'title, body, comments',
     group: 'general',
+    suggestions: [
+      { name: 'title', prefix: 'in:' },
+      { name: 'body', prefix: 'in:' },
+      { name: 'comments', prefix: 'in:' },
+    ],
   },
   {
     name: 'state:',
     tip: 'open, closed',
     group: 'general',
+    suggestions: [
+      { name: 'open', prefix: 'state:' },
+      { name: 'closed', prefix: 'state:' },
+    ],
   },
   {
     name: 'label:',
@@ -26,11 +39,21 @@ const filters = [
     name: 'is:',
     tip: 'open, closed, merged',
     group: 'general',
+    suggestions: [
+      { name: 'open', prefix: 'is:' },
+      { name: 'closed', prefix: 'is:' },
+      { name: 'merged', prefix: 'is:' },
+    ],
   },
   {
     name: 'no:',
     tip: 'label, milestone, assignee',
     group: 'general',
+    suggestions: [
+      { name: 'label', prefix: 'no:' },
+      { name: 'milestone', prefix: 'no:' },
+      { name: 'assignee', prefix: 'no:' },
+    ],
   },
   {
     name: 'language:',
@@ -41,11 +64,40 @@ const filters = [
     name: 'status:',
     tip: 'pending, failure, success',
     group: 'general',
+    suggestions: [
+      { name: 'pending', prefix: 'status:' },
+      { name: 'failure', prefix: 'status:' },
+      { name: 'success', prefix: 'status:' },
+    ],
   },
   {
     name: 'sort:',
     tip: 'comments, created, updated, reactions, interactions',
     group: 'general',
+    suggestions: [
+      { name: 'comments', prefix: 'sort:' },
+      { name: 'created', prefix: 'sort:' },
+      { name: 'updated', prefix: 'sort:' },
+      { name: 'reactions', prefix: 'sort:' },
+      { name: 'reactions-+1', prefix: 'sort:' },
+      { name: 'reactions--1', prefix: 'sort:' },
+      { name: 'reactions-smile', prefix: 'sort:' },
+      { name: 'reactions-thinking_face', prefix: 'sort:' },
+      { name: 'reactions-heart', prefix: 'sort:' },
+      { name: 'reactions-tada', prefix: 'sort:' },
+      { name: 'interactions', prefix: 'sort:' },
+      { name: 'comments-desc', prefix: 'sort:' },
+      { name: 'created-desc', prefix: 'sort:' },
+      { name: 'updated-desc', prefix: 'sort:' },
+      { name: 'reactions-desc', prefix: 'sort:' },
+      { name: 'reactions-+1-desc', prefix: 'sort:' },
+      { name: 'reactions--1-desc', prefix: 'sort:' },
+      { name: 'reactions-smile-desc', prefix: 'sort:' },
+      { name: 'reactions-thinking_face-desc', prefix: 'sort:' },
+      { name: 'reactions-heart-desc', prefix: 'sort:' },
+      { name: 'reactions-tada-desc', prefix: 'sort:' },
+      { name: 'interactions-desc', prefix: 'sort:' },
+    ],
   },
   {
     name: 'milestone:',
@@ -223,9 +275,27 @@ class FilterSuggester extends React.Component {
     }
     const segments = value.split(/\s+/)
     const lastValue = segments[segments.length - 1].replace(/^-/, '')
-    const length = lastValue.length
+    let length = lastValue.length
     if (length < 1) {
       return []
+    }
+    if (lastValue.match(/:$/)) {
+      const filter = filters.filter(s => s.name === lastValue)[0]
+      if (filter && typeof filter.suggestions === 'object') {
+        return filter.suggestions
+      }
+    }
+    const colonIndex = lastValue.indexOf(':')
+    if (colonIndex > -1) {
+      const name = lastValue.slice(0, colonIndex + 1)
+      const filter = filters.filter(s => s.name === name)[0]
+      if (filter && typeof filter.suggestions === 'object') {
+        const lastFilterValue = lastValue.slice(colonIndex + 1)
+        length = lastFilterValue.length
+        return filter.suggestions.filter(s =>
+          s.name.slice(0, length) === lastFilterValue
+        )
+      }
     }
     return filters.filter(s => s.name.slice(0, length) === lastValue)
   }
@@ -233,11 +303,17 @@ class FilterSuggester extends React.Component {
   getSuggestionValue(filter) {
     const segments = this.state.value.split(/\s+/)
     if (segments.length === 1) {
+      if (typeof filter.prefix === 'string') {
+        return `${filter.prefix}${filter.name}`
+      }
       return filter.name
     }
     const lastValue = segments[segments.length - 1]
     const priorQuery = segments.slice(0, segments.length - 1).join(' ')
     let newSuggestion = filter.name
+    if (typeof filter.prefix === 'string') {
+      newSuggestion = `${filter.prefix}${newSuggestion}`
+    }
     if (lastValue.indexOf('-') === 0) {
       newSuggestion = `-${newSuggestion}`
     }
@@ -247,7 +323,15 @@ class FilterSuggester extends React.Component {
   renderSuggestion(suggestion) {
     return (
       <span className="suggestion">
-        <strong>{suggestion.name}</strong> &mdash; {suggestion.tip}
+        {typeof suggestion.prefix === 'string' ? (
+          <span>
+            <span>{suggestion.prefix}</span><strong>{suggestion.name}</strong>
+          </span>
+        ) : (
+          <span>
+            <strong>{suggestion.name}</strong> &mdash; {suggestion.tip}
+          </span>
+        )}
       </span>
     )
   }
