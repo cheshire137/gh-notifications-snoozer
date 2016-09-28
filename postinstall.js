78823eab8f20286879e63d2b92773852e92959ec
Sarah Vessels <cheshire137@gmail.com>
2016-09-28 11:32:01 -0500

    Whoops don't overwrite existing config.json

--- postinstall.js
+++ postinstall.js
@@ -2,8 +2,13 @@ const fs = require('fs')
 const path = require('path')
 
 const configPath = path.join('src', 'config.json')
-console.log('checking for ' + configPath + '...')
-fs.access(configPath, fs.F_OK, function(err) {
-  console.log('copying config.json.example to ' + configPath)
-  fs.createReadStream('config.json.example').pipe(fs.createWriteStream(configPath))
+console.log(`checking for ${configPath}...`)
+fs.access(configPath, fs.F_OK, err => {
+  if (err) {
+    console.log(`copying config.json.example to ${configPath}`)
+    fs.createReadStream('config.json.example').
+       pipe(fs.createWriteStream(configPath))
+  } else {
+    console.log(`${configPath} already exists`)
+  }
 })
