430217908042bfeb41b48b68ba550f83d6db27ae
probablycorey <probablycorey@github.com>
2016-10-08 13:23:34 -0700

    Add filter reducer

--- src/reducers/FiltersReducer.js
+++ src/reducers/FiltersReducer.js
@@ -1,13 +1,21 @@
-const filters_add = (filters, action) => {
+const filtersUpdate = (filters, action) => {
   const newFilter = {}
   newFilter[action.name] = action.query
   return Object.assign({}, filters, newFilter)
 }
 
+const filtersRemove = (filters, action) => {
+  const updatedFilters = Object.assign({}, filters)
+  delete updatedFilters[action.name]
+  return updatedFilters
+}
+
 module.exports = (filters = {}, action) => {
   switch (action.type) {
-    case 'FILTERS_ADD':
-      return filters_add(filters, action)
+    case 'FILTERS_UPDATE':
+      return filtersUpdate(filters, action)
+    case 'FILTERS_REMOVE':
+      return filtersRemove(filters, action)
     default:
       return filters
   }
--- test/reducers/FilterReducerTest.js
+++ test/reducers/FilterReducerTest.js
@@ -9,18 +9,40 @@ describe.only('Filters reducer', () => {
     assert.deepEqual({ }, store.getState())
   })
 
-  describe('FILTERS_ADD', () => {
+  describe('FILTERS_UPDATE', () => {
     it('adds a filter', () => {
       const initialFilters = { 'first filter': 'label:first' }
       const store = Redux.createStore(FiltersReducer, initialFilters)
 
-      store.dispatch({ type: 'FILTERS_ADD', name: 'new filter', query: 'label:new' })
+      store.dispatch({ type: 'FILTERS_UPDATE', name: 'new filter', query: 'label:new' })
       const expectedFilters = {
         'first filter': 'label:first',
         'new filter': 'label:new',
       }
 
       assert.deepEqual(expectedFilters, store.getState())
+    })
+
+    it('updates an existing filter', () => {
+      const initialFilters = { 'first filter': 'label:first' }
+      const store = Redux.createStore(FiltersReducer, initialFilters)
+
+      store.dispatch({ type: 'FILTERS_UPDATE', name: 'first filter', query: 'label:updated' })
+      const expectedFilters = {
+        'first filter': 'label:updated',
+      }
+
+      assert.deepEqual(expectedFilters, store.getState())
+    })
+  })
+
+  describe('FILTERS_REMOVE', () => {
+    it('removes a filter', () => {
+      const initialFilters = { 'first filter': 'label:first' }
+      const store = Redux.createStore(FiltersReducer, initialFilters)
+
+      store.dispatch({ type: 'FILTERS_REMOVE', name: 'first filter' })
+      assert.deepEqual({ }, store.getState())
     })
   })
 })
