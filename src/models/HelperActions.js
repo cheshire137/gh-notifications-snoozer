40716b21072764377bd7b8801784eca664f12476
probablycorey <probablycorey@github.com>
2016-11-18 13:32:37 -0800

    Pull out task updating

---
+++ src/models/HelperActions.js
@@ -0,0 +1,16 @@
+const GitHub = require('./GitHub')
+
+module.exports = class HelperActions {
+  static updateTasks(dispatch, filter) {
+    const github = new GitHub()
+    github.getTasks(filter).then(result => {
+      const { tasks } = result
+
+      // GitHub's api doesn't like when the output includes milliseconds
+      const updatedAt = (new Date()).toISOString().replace(/\.\d{3}Z/, 'Z')
+      const updatedFilter = Object.assign({}, filter, { updatedAt })
+      dispatch({ type: 'FILTERS_UPDATE', filter: updatedFilter })
+      dispatch({ type: 'TASKS_UPDATE', filter, tasks })
+    })
+  }
+}
